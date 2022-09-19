ea6b1eceba863d40ae481453faf5e2cff6e9ff04
achmdfurqon <achmdfurqon@gmail.com>
2022-09-19 10:37:22 +0700

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('title', 'Home')
 @section('container')
-    <div id="carouselExampleDark" class="carousel carousel-dark slide mb-3" data-bs-ride="carousel" style="height: 450px;">
+    <div id="carouselExampleDark" class="carousel carousel-dark slide mb-3" data-bs-ride="carousel" style="height: 540px;">
         <div class="carousel-indicators">
             @if ($totalAgenda == 0)
                 <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
@@ -40,71 +40,58 @@
         </div>
         <div class="carousel-inner">
             @forelse ($todayEvent as $event)
-                <div class="carousel-item @if ($loop->iteration == 1) active @endif" data-bs-interval="3000">
+                <div class="carousel-item @if ($loop->iteration == 1) active @endif" data-bs-interval="5000">
                     <div class="d-block w-300">
                         <h3 class="text-center mb-5">Agenda Hari Ini</h3>
-                        <div class="row">
+                        <!-- <div class="row"> -->
                             <div class="row d-flex justify-content-center">
-                                <div class="col-7">
-                                    <div class="event overflow-auto bg-white">
+                                <!-- <div class="col-9"> -->
+                                    <!-- <div class="event overflow-auto bg-white"> -->
                                         <div class="desc">
-                                            {{-- <h3>{{ $event->peserta }} - {{ $event->title }}</h3> --}}
-                                            <div class="row h5">
-                                                <div class="col-6">
-                                                    Event
-                                                </div>
-                                                <div class="col-6">
-                                                    : {{ $event->title }}
+                                            <div class="row h1">
+                                                <div class="col-12 text-center">
+                                                    {{ $event->title }}
                                                 </div>
                                             </div>
-                                            <div class="row h5">
-                                                <div class="col-6">
-                                                    Lokasi
-                                                </div>
-                                                <div class="col-6">
-                                                    : {{ $event->location }}
+                                            <div class="row h2">
+                                                <div class="col-12 text-center">
+                                                    {{ $event->location }}
                                                 </div>
                                             </div>
-                                            <div class="row h5">
-                                                <div class="col-6">
-                                                    Penyelenggara
-                                                </div>
-                                                <div class="col-6">
-                                                    : {{ $event->penyelenggara }}
+                                            <div class="row h1">
+                                                <div class="col-12 text-center">
+                                                    {{ $event->penyelenggara }}
                                                 </div>
                                             </div>
-                                            <div class="row h5">
-                                                <div class="col-6">
-                                                    Tanggal
-                                                </div>
-                                                <div class="col-6">
-                                                    : {{ \Carbon\Carbon::parse($event->start)->translatedFormat('l, d F Y') }}
+                                            <div class="row h3">
+                                                <div class="col-12 text-center">
+                                                    {{ \Carbon\Carbon::parse($event->start)->translatedFormat('l, d F Y') }}
                                                 </div>
                                             </div>
                                         </div>
-                                        <div class="date float-end">
+                                        <!-- <div class="date float-end">
                                             <div class="day">{{ \Carbon\Carbon::parse($event->start)->format('d') }}
                                             </div>
                                             <div class="month">
                                                 {{ \Carbon\Carbon::parse($event->start)->translatedFormat('F') }}</div>
-                                        </div>
-                                    </div>
-                                </div>
+                                        </div> -->
+                                    <!-- </div> -->
+                                <!-- </div> -->
                             </div>
-                        </div>
+                        <!-- </div> -->
                     </div>
                 </div>
             @empty
-                <div class="carousel-item active" data-bs-interval="3000">
+                <div class="carousel-item active" data-bs-interval="5000">
                     <div class="d-block w-100">
                         <h3 class="text-center mb-5">Agenda Hari Ini</h3>
-                        <div class="row">
+                        <!-- <div class="row"> -->
                             <div class="row d-flex justify-content-center">
-                                <div class="col-7">
-                                    <h6 class="text-center">Belum Ada Agenda</h6>
-                                </div>
+                                <!-- <div class="col-7"> -->
+                                    <h3 class="text-center">Belum Ada Agenda</h6>
+                                <!-- </div> -->
                             </div>
-                        </div>
+                        <!-- </div> -->
                     </div>
                 </div>
             @endforelse
@@ -114,20 +101,42 @@
                         <h3 class="text-center mb-5">Agenda Akan Datang</h3>
                         <div class="row mt-5">
                             <div class="row d-flex justify-content-center">
-                                <div class="col-7">
-                                    <div class="event overflow-auto bg-white">
-                                        <div class="desc">
+                                <div class="desc">
+                                    <div class="row h1">
+                                        <div class="col-12 text-center">
+                                            {{ $event->title }}
+                                        </div>
+                                    </div>
+                                    <div class="row h2">
+                                        <div class="col-12 text-center">
+                                            {{ $event->location }}
+                                        </div>
+                                    </div>
+                                    <div class="row h1">
+                                        <div class="col-12 text-center">
+                                            {{ $event->penyelenggara }}
+                                        </div>
+                                    </div>
+                                    <div class="row h3">
+                                        <div class="col-12 text-center">
+                                            {{ \Carbon\Carbon::parse($event->start)->translatedFormat('l, d F Y') }}
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- <div class="col-7">
+                                    <div class="event overflow-auto bg-white"> -->
+                                        <!-- <div class="desc">
                                             <h1>{{ $event->peserta }} - {{ $event->title }}</h1>
                                             <span class="fa fa-map-marker"></span> {{ $event->location }}
-                                        </div>
-                                        <div class="date float-end">
+                                        </div> -->
+                                        <!-- <div class="date float-end">
                                             <div class="day">{{ \Carbon\Carbon::parse($event->start)->format('d') }}
                                             </div>
                                             <div class="month">
                                                 {{ \Carbon\Carbon::parse($event->start)->translatedFormat('F') }}</div>
-                                        </div>
-                                    </div>
-                                </div>
+                                        </div> -->
+                                    <!-- </div>
+                                </div> -->
                             </div>
                         </div>
                     </div>
@@ -136,24 +145,24 @@
                 <div class="carousel-item" data-bs-interval="3000">
                     <div class="d-block w-100">
                         <h3 class="text-center mb-5">Agenda Akan Datang</h3>
-                        <div class="row">
+                        <!-- <div class="row"> -->
                             <div class="row d-flex justify-content-center">
-                                <div class="col-7">
-                                    <h6 class="text-center">Belum Ada Agenda</h6>
-                                </div>
+                                <!-- <div class="col-7"> -->
+                                    <h3 class="text-center">Belum Ada Agenda</h6>
+                                <!-- </div> -->
                             </div>
-                        </div>
+                        <!-- </div> -->
                     </div>
                 </div>
             @endforelse
-        </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
+        </div>        
     </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
 @endsection
